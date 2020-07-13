40bf80e7f1338f91217d3e77c67bcfcf7d3650d3
herypank <60302361+herypank@users.noreply.github.com>
2020-07-13 13:02:28 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from datetime import datetime as dt
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
+now = dt.now()
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+
+rendered_page = template.render(
+    year_count = now.year - 1920
+)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
