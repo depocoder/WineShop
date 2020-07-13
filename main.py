ed7184f0e09278d26bdc3e5cf23db5c528ac52d5
herypank <60302361+herypank@users.noreply.github.com>
2020-07-13 15:04:11 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from datetime import datetime as dt
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+import pandas
 
+excel_data_df = pandas.read_excel('wines.xlsx')
 now = dt.now()
 
 env = Environment(
@@ -12,7 +14,9 @@ env = Environment(
 template = env.get_template('template.html')
 
 rendered_page = template.render(
-    year_count = now.year - 1920
+    year_count = now.year - 1920,
+    wines = excel_data_df.to_dict(orient='record')
+
 )
 
 with open('index.html', 'w', encoding="utf8") as file:
