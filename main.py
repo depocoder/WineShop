b6c1885a0f6cf00cdc8c504cf4897f8a4f31c5b6
herypank <60302361+herypank@users.noreply.github.com>
2020-07-13 17:35:24 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,9 +2,26 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from datetime import datetime as dt
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import pandas
+from pprint import pprint
 
 excel_data_df = pandas.read_excel('wines.xlsx')
 now = dt.now()
+wines = excel_data_df.to_dict(orient='record')
+sorted_wines = {
+    'Белые вина':{}, 
+    'Красные вина':{},
+    'Напитки':{}}
+
+
+
+for item in wines:
+    if item['Категория']=='Белые вина':
+        sorted_wines['Белые вина'] =  sorted_wines['Белые вина'] + item 
+    elif item['Категория']=='Красные вина':
+        sorted_wines['Красные вина'] = sorted_wines['Красные вина'] + item 
+    else:
+        sorted_wines['Напитки'] = item
+print(sorted_wines)
 
 env = Environment(
     loader=FileSystemLoader('.'),
@@ -15,7 +32,7 @@ template = env.get_template('template.html')
 
 rendered_page = template.render(
     year_count = now.year - 1920,
-    wines = excel_data_df.to_dict(orient='record')
+    
 
 )
 
