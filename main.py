695147e17afd046dccb0b16d31c9c7605709e87d
herypank <60302361+herypank@users.noreply.github.com>
2020-07-14 17:40:34 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from datetime import datetime as dt
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import pandas
+from pprint import pprint
 from collections import defaultdict
 
 
 excel_data_df = pandas.read_excel('wines.xlsx')
-wines_data = excel_data_df.to_dict(orient='record')
+now = dt.now()
+wines = excel_data_df.to_dict(orient='record')
 sorted_wines =  defaultdict(list)
 
-for wine in wines_data:
+for wine in wines:
     sorted_wines[wine['Категория']].append(wine)
 
         
@@ -21,11 +23,10 @@ env = Environment(
 
 template = env.get_template('template.html')
 
-now = dt.now()
 rendered_page = template.render(
-    year_counter = now.year - 1920,
-    wines = (sorted_wines),
-    сatalog = sorted(sorted_wines)
+    year_count = now.year - 1920,
+    winess = (sorted_wines),
+    categories = sorted(sorted_wines)
     
 
 )
